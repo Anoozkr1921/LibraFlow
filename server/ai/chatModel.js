7bb0c3f840bccf9e6ed98354fb228e1a3f27c5f5
Anoozkr1921 <anoozburnwal1921@gmail.com>
2026-09-03 02:21:22 +0530

    Updated the Gemini model

--- server/ai/chatModel.js
+++ server/ai/chatModel.js
@@ -1,6 +1,6 @@
 const {ChatGoogleGenerativeAI} = require("@langchain/google-genai") ;
 const chatModel = new ChatGoogleGenerativeAI({
-    model: "gemini-2.5-flash",
+    model: "gemini-3.6-flash",
     apiKey: process.env.GEMINI_API_KEY,
     temperature: 0.3,
 });
