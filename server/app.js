c01be50728ae7d5a90a8add1f2ee672d9523de73
Anoozkr1921 <anoozburnwal1921@gmail.com>
2026-09-03 23:09:31 +0530

    Updated fontend

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const conversationRoutes = require("./routes/conversationRoutes");
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
 }));
 
